1252864f9b71d389b1d550a389f8f058f8ac4597
Jesse Odion <jesse12odion@gmail.com>
2025-04-16 20:28:56 +0100

    Exercise 1A started

---
+++ src/Shape.java
@@ -0,0 +1,17 @@
+public abstract class Shape {
+    private int shapeSize;
+
+    public Shape(int shapeSize) {
+        this.shapeSize = shapeSize;
+    }
+    //Getter Method
+    public int getShapeSize() {
+        return shapeSize;
+    }
+    //Setter Method
+    public void setShapeSize(int shapeSize) {
+        this.shapeSize = shapeSize;
+    }
+    //This is for the ASCII art representation
+    public abstract void draw();
+}
