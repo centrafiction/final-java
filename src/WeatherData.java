c91f1886d534cc0215f167a684548be480e6e96b
Jesse Odion <jesse12odion@gmail.com>
2025-04-17 23:45:53 +0100

    Exercise 2A Started

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,2 +1,9 @@
 public class WeatherData {
+    private int year;
+    private String month;
+    private float maxTemp;
+    private float minTemp;
+    private float rainAmount;
+    private float meanTemp;
+
 }
