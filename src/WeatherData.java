4f005844e033eb80fd60def8db3b1e8872af8ccf
Jesse Odion <jesse12odion@gmail.com>
2025-04-18 16:12:20 +0100

    Exercise 2B Started

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,9 +1,41 @@
 public class WeatherData {
     private int year;
-    private String month;
+    private int month;
     private float maxTemp;
     private float minTemp;
     private float rainAmount;
     private float meanTemp;
 
+    public WeatherData(int year, int month, float maxTemp, float minTemp, float rainAmount){
+        this.year = year;
+        this.month = month;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.rainAmount = rainAmount;
+        this.meanTemp = (maxTemp + minTemp) / 2;
+    }
+
+    // Getter Methods
+    public int getYear() {
+        return year;
+    }
+    public int getMonth() {
+        return month;
+    }
+
+    public float getMaxTemp() {
+        return maxTemp;
+    }
+
+    public float getMeanTemp() {
+        return (maxTemp + minTemp)/2;
+    }
+
+    public float getMinTemp() {
+        return minTemp;
+    }
+
+    public float getRainAmount() {
+        return rainAmount;
+    }
 }
--- src/WeatherDataIO.java
+++ src/WeatherDataIO.java
@@ -1,2 +1,21 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class WeatherDataIO {
+    public static ArrayList<WeatherData> readCSV(String fileName){
+        ArrayList<WeatherData> listWeather = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            if ((line = reader.readLine()) != null && line.startsWith("Year")) {
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found" + fileName);
+        } catch (IOException e) {
+            System.err.println("Error reading" + e.getMessage());
+        }
+    }
 }
