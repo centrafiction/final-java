7d7f11e746d9a142e09779996a70747fd3dd6f3d
Jesse Odion <jesse12odion@gmail.com>
2025-04-18 16:54:36 +0100

    Exercise 2B Continued

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,4 +1,5 @@
 public class Exercise2 {
+    // The original heathrow.csv has the amount of rain in the 907th line empty justt to remind you for the future
     public static void main(String[] args) {
         WeatherDataIO.readCSV("Heathrow.csv");
     }
