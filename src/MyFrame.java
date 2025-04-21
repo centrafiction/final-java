a78a8a0a685daaa1d2701011c33649dd548ac29e
Jesse Odion <jesse12odion@gmail.com>
2025-04-21 13:33:22 +0100

    Exercise3 started, changed files structure and tinkering with frame design and text display

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -8,30 +8,38 @@ public class MyFrame extends JFrame{
         this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //EXIT OUT OF APPLICATION
         //this.setResizable(false);
+        this.setLayout(null);
+        this.setPreferredSize(new Dimension(800, 500));
         this.getContentPane().setBackground(Color.orange); //Changes background colour
 
 
         //this.add(new Component());
         //lets the component class attributes be used in the frame
-        this.setVisible(true);
         //has the this appear
         JTextArea textArea = new JTextArea();
 
         textArea.setEditable(false);
         textArea.setFocusable(false);
-        //textArea.setOpaque(false);              // make it truly transparent
-        //textArea.setBackground(new Color(0,0,0,0));
+        textArea.setOpaque(true);              // make it truly transparent
+        textArea.setBackground(Color.orange);
         textArea.setBorder(null);
         textArea.setFont(new JLabel().getFont()); // matches JLabel font
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
 
         JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(800, 600));
+        scrollPane.getViewport().setOpaque(false);
+        scrollPane.setOpaque(false);
+        scrollPane.setBorder(null);
 
         this.add(scrollPane);
         this.setLayout(new BorderLayout());
         this.add(scrollPane, BorderLayout.CENTER);
+
+        JButton button = new JButton("Refresh");
+        this.add(button, BorderLayout.SOUTH);
+
+
 
 
         OpenMeteoClient.updateCurrentWeather("Paris");
@@ -51,8 +59,10 @@ public class MyFrame extends JFrame{
                     .append("mm\n");
         }
 
-        textArea.setText(forecastText.toString());
+        //textArea.setText(forecastText.toString());
         this.pack();
+        this.setLocationRelativeTo(null); // optional: center on screen
+        this.setVisible(true);
     }
 }
 
